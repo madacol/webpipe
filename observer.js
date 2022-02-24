7cda4d8cfcabb8c38fe694b39d10efb4dddabbe6
Marco D'Agostini <madacol10@gmail.com>
2022-02-25 00:36:01 +0100

    Support observing input and textarea elements by polling it :puke:

--- observer.js
+++ observer.js
@@ -5,20 +5,37 @@ function observeNode(node) {
     const payload = {
         action: "create",
         idx,
-        textContent: node.textContent
+        textContent: (node.value || node.textContent)
     }
-    browser.runtime.sendMessage(payload, () => {
-        const observer = new MutationObserver(function (mutationsList, observer) {
-            const payload = {
-                action: "update",
-                idx,
-                textContent: node.textContent
-            }
-            browser.runtime.sendMessage(payload)
+    function sendUpdate(text) {
+        const payload = {
+            action: "update",
+            idx,
+            textContent: text
+        }
+        browser.runtime.sendMessage(payload)
+    }
+    if (node.tagName === "INPUT" || node.tagName === "TEXTAREA") {
+        browser.runtime.sendMessage(payload, ()=>{
+            /**
+             * There's no clean way to detect an `input.value` change from an extension =/
+             * https://stackoverflow.com/questions/32383349/detect-input-value-change-with-mutationobserver
+             */
+            let oldValue = node.value
+            setInterval(()=>{
+                if (oldValue === node.value) return
+
+                sendUpdate(node.value)
+                oldValue = node.value
+            }, 100)
         })
-        observer.observe(node, { childList: true, subtree: true, characterData: true })
-        observers[idx].observer = observer
-    })
+    } else {
+        browser.runtime.sendMessage(payload, () => {
+            const observer = new MutationObserver(()=>sendUpdate(node.textContent))
+            observer.observe(node, { childList: true, subtree: true, characterData: true })
+            observers[idx].observer = observer
+        })
+    }
 }
 
 export default async function observe() {
