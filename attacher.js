2d79337489ddd846abc0894eb3ab0736ca718da6
Marco D'Agostini <madacol10@gmail.com>
2022-02-24 20:49:53 +0100

    attach now works on state frameworks and contenteditable rich editors

--- attacher.js
+++ attacher.js
@@ -1,9 +1,57 @@
 const elementsAttached = []
 
+function selectNodeText(node) {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+export async function update({textContent, idx}) {
+    const node = elementsAttached[idx]
+    node.value = textContent
+
+    /**
+     * The following is to make it work for elements used by rich-editors,
+     * fancy state frameworks, contenteditable, blah blah
+     * 
+     * https://github.com/facebook/draft-js/issues/616
+     */
+    node.dispatchEvent(new KeyboardEvent("keyup"))
+    node.dispatchEvent(new KeyboardEvent("keypress"))
+    node.dispatchEvent(new KeyboardEvent("keydown"))
+    node.dispatchEvent(new InputEvent("input"))
+    node.dispatchEvent(new InputEvent("change"))
+    node.dispatchEvent(new InputEvent("blur"))
+    node.dispatchEvent(new ClipboardEvent(
+        "paste",
+        {dataType: "text/plain", data: textContent}
+    )) // https://github.com/facebook/draft-js/issues/616#issuecomment-426047799
+    if (node.isContentEditable) {
+        /**
+         * First let me send a compassionate message full of love to all poor souls
+         * who had no choice but to use the `contenteditable` attribute,
+         * and to all those like me who are forced to work with it against our will ='(
+         * 
+         * Now let's use a deprecated API yaaayyy (this is needed for firefox which probably will affect chrome too)
+         * https://stackoverflow.com/questions/60581285/execcommand-is-now-obsolete-whats-the-alternative
+         * TL;DR: THERE ARE NO ALTERNATIVES!
+         * 
+         * Now pray for my self from the future who will have to deal with duplicated event triggers
+         * and draftjs braking apart because its really sensitive.
+         * 
+         * Really sorry for this rudeness but, Here we go! <3
+         */
+        selectNodeText(node)
+        document.execCommand('insertText', false, textContent)
+    }
+}
+
 export async function attach({observer}) {
     function attachPipe(node) {
-        node.value = observer.textContent
         const idx = elementsAttached.push(node) - 1
+        update({textContent: observer.textContent, idx})
         const payload = {
             action: "attach",
             idx,
@@ -13,10 +61,4 @@ export async function attach({observer}) {
     }
     (await import(browser.runtime.getURL("element-picker.js"))) // Injects `elementPicker` into `window`
     window.elementPicker.init({onClick: attachPipe, backgroundColor: "#ffbb0070"})
-}
-
-
-export async function update({textContent, idx}) {
-    const node = elementsAttached[idx]
-    node.value = textContent
 }
