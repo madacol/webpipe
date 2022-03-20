16a6a14406b865b7423298636910213832263ba4
Marco D'Agostini <madacol10@gmail.com>
2022-03-20 02:27:19 +0100

    New interface to easily add/remove/edit selectors while picking element

---
+++ src/CheckboxToggle.svelte
@@ -0,0 +1,90 @@
+<script>
+    // export let /**@type {boolean}*/ checked;
+    export let /**@type {[]}*/ checked;
+    export let /**@type {string}*/ label;
+</script>
+
+<div>
+    <label>
+        <input
+            on:change
+            bind:checked
+            value={label}
+            type="checkbox"
+        >
+        <div>
+            <span/>
+        </div>
+        {label}&nbsp;
+    </label>
+</div>
+
+<style>
+
+div {
+    display: flex;
+    justify-content: center;
+}
+
+label {
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+/* Hide default HTML checkbox */
+input {
+    position: absolute;
+    opacity: 0;
+    width: 0;
+    height: 0;
+}
+
+label > div {
+    display: inline-block;
+    position: relative;
+    width: 2.4em;
+    height: 1.3em;
+}
+
+/* The slider */
+span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(99, 99, 99);
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 1.3em;
+}
+
+span:before {
+    position: absolute;
+    content: "";
+    height: 1em;
+    width: 1em;
+    left: 0.15em;
+    bottom: 0.15em;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+}
+
+input:checked + div > span {
+    background-color: rgb(95, 95, 51);
+}
+
+input:focus + div > span {
+    box-shadow: 0 0 1px yellow;
+}
+
+input:checked + div > span:before {
+    -webkit-transform: translateX(1em);
+    -ms-transform: translateX(1em);
+    transform: translateX(1em);
+}
+</style>
--- src/ObserverPreview.svelte
+++ src/ObserverPreview.svelte
@@ -1,53 +1,207 @@
 <script>
-    /**
-     * get coords of any element relative to the document
-     * simplified version of https://stackoverflow.com/a/26230989/3163120
-     * @param {HTMLElement} elem
-     */
-    function getCoords(elem) { // crossbrowser version
-        const box = elem.getBoundingClientRect();
-        const top  = box.top  + window.scrollY;
-        const left = box.left + window.scrollX;
-        return { top: Math.round(top), left: Math.round(left) };
-    }
+import CheckboxToggle from "./CheckboxToggle.svelte";
 
     /**@type {HTMLElement}*/ 
     export let hoveringNode = null;
-    /** @type {number}*/ let left = 0;
-    /** @type {number}*/ let top  = 0;
-    $: if (hoveringNode) {
-        ({left, top} = getCoords(hoveringNode))
+    /**@type {string}*/ 
+    export let cssSelector = "";
+
+    /**
+     * get coords of any element relative to the document
+     * simplified version of https://stackoverflow.com/a/26230989/3163120
+     * @param {HTMLElement} node
+     * @returns {{
+     *  top: number,
+     *  left: number,
+     *  bottom: number,
+     *  right: number,
+     * }}
+     */
+    function getCoords(node) { // crossbrowser version
+        if (!node) return
+        const box = node.getBoundingClientRect();
+        const top  = box.top  + window.scrollY;
+        const left = box.left + window.scrollX;
+        const bottom = box.bottom + window.scrollY;
+        const right = box.right + window.scrollX;
+        return {
+            top: Math.round(top),
+            left: Math.round(left),
+            bottom: Math.round(bottom),
+            right: Math.round(right),
+        };
     }
 
-    /** @type {boolean}*/ let isActive = false;
-    /** @type {[string]}*/ let classes=[];
-    /** @type {[string]}*/ let attributes=[];
+    $: nodeCoords = getCoords(hoveringNode)
+
+    // regex to extract selectors
+    const tagRegex = /[a-z\-]+/g
+    const idRegex = /#[a-zA-Z0-9\-_]+/g
+    const classRegex = /\.[a-zA-Z0-9\-_]+/g
+    const attrRegex = /\[[a-zA-Z0-9\-_]+(?:='.+?')?\]/g
+    const pseudoClassRegex = /\:[a-z\-]+(?:\([^\)]+\))?/g
+    const anySelectorRegex = new RegExp(
+        `${tagRegex.source}|${idRegex.source}|${classRegex.source}|${attrRegex.source}|${pseudoClassRegex.source}`,
+        'g'
+    )
+
+    let /**@type {string}*/ ancestorsSelector;
+    let /**@type {string}*/ nodeSelector;
+    $: if (cssSelector) {
+        const wholeNodeSelectorRegex = new RegExp(`(?:${anySelectorRegex.source})+`,'g')
+        const nodeSelectorMatch = Array.from(cssSelector.matchAll(wholeNodeSelectorRegex)).at(-1)
+        console.log(cssSelector, nodeSelectorMatch);
+        nodeSelector = nodeSelectorMatch[0]
+        ancestorsSelector = cssSelector.slice(0, nodeSelectorMatch.index)
+    }
+    let isId = false;
+    let isTag = false;
+    let selectedClasses=[];
+    let selectedAttributes=[];
+    let selectedPseudoClasses=[];
+    /**
+     * Extract individual selectors from `nodeSelector` to populate following states
+     * `isId`
+     * `isTag`
+     * `selectedClasses`
+     * `selectedAttributes`
+     * `selectedPseudoClasses`
+     */
+    function parseNodeSelector() {
+        isId = false;
+        isTag = false;
+        selectedClasses=[];
+        selectedAttributes=[];
+        selectedPseudoClasses=[];
+        for (const match of nodeSelector.matchAll(anySelectorRegex)) {
+            const selector = match[0]
+            switch (selector[0]) {
+                case "#": isId = true; break;
+                case ".": selectedClasses.push(selector); break;
+                case "[": selectedAttributes.push(selector.replaceAll("\\","")); break;
+                case ":": selectedPseudoClasses.push(selector); break;
+                default:
+                    (selector === hoveringNode?.tagName.toLowerCase())
+                        ? isTag = true
+                        : console.error(`selector format unkown: ${selector}`);
+                    break;
+            }
+        }
+    }
+    $: if (nodeSelector) parseNodeSelector()
+
+    /**
+     * Below we do the checkbox bind:groups but traversing components
+     * https://github.com/sveltejs/svelte/issues/2308#issuecomment-1005942571
+     * https://svelte.dev/repl/02d60142a1cc470bb43e0cfddaba4af1?version=3.38.3
+     */
+
+
+    $: id = isId ? hoveringNode?.id : "";
+    $: tag = isTag ? hoveringNode?.tagName.toLowerCase() : "";
+    $: classes = Array.from(hoveringNode?.classList || []).filter(x=>x!=="element-picking").map(x=>"."+x)
+
+    // attributes
+    $: attributes = Object.values(hoveringNode?.attributes || [])
+                          .filter(attr=>!["class","style"].includes(attr.name))
+                          .map(attr=>`[${attr.name}='${attr.value}']`)
+
+    $: constructedNodeSelector = tag + id + selectedClasses.join("") + selectedAttributes.join("") + selectedPseudoClasses.join("")
+    $: selector = ancestorsSelector + constructedNodeSelector
+    $: {
+        document.querySelectorAll(".element-picking").forEach(x=>x.classList.remove("element-picking"))
+        try {
+            document.querySelectorAll(selector).forEach(x=>x.classList.add("element-picking"))
+        } catch (error) {console.error(error);}
+    }
+    $: console.log(selector);
+
+    let isActive = false;
+
+    function updateGroup({ target }, group) {
+        const { value, checked } = target;
+        return (checked)
+            ? [...group, value]
+            : group.filter((item) => item !== value);
+    }
+
+    // group.includes(label)
 </script>
 
 {#if hoveringNode}
-    <div id="floatingButton" style="
-        left: {left}px;
-        top: {top}px;
+    {#if isActive}
+        <div id="shadow-cancel" style="top: {window.scrollY}px;" on:click={()=>{
+            isActive=false
+            document.querySelectorAll(".element-picking").forEach(x=>x.classList.remove("element-picking"))
+        }}/>
+    {/if}
+    <div id="hoveringNode-position" style="
+        left: {nodeCoords.left}px;
+        top: {Math.max(nodeCoords.top, window.scrollY)}px;
     ">
-        <button on:click={()=>isActive=!isActive}>
-            @
-        </button>
         {#if isActive}
-            {#each hoveringNode.classList as classString }
-                <span>{classString}</span>
-                <input type="checkbox" bind:group={classes} value={classString}>
-            {/each}
-            <hr>
-            {#each hoveringNode.attributes as {name, value} }
-                <span>{`[${name}=${value}]`}</span>
-                <input type="checkbox" bind:group={attributes} value={`[${name}=${value}]`}>
-            {/each}
+            <div id="selector">
+                <input type="text" bind:value={selector}>
+
+                <CheckboxToggle
+                    label={hoveringNode.tagName.toLowerCase()}
+                    bind:checked={isTag}
+                />
+                {#if hoveringNode.id}
+                    <CheckboxToggle
+                        label={hoveringNode.id}
+                        bind:checked={isId}
+                    />
+                {/if}
+                <hr>
+                {#each classes as classString }
+                    <div class="classes">
+                        <CheckboxToggle
+                            label={classString}
+                            checked={selectedClasses.includes(classString)}
+                            on:change={e=>selectedClasses = updateGroup(e, selectedClasses)}
+                        />
+                    </div>
+                {/each}
+                <hr>
+                {#each attributes as selector }
+                    <div class="attributes">
+                        <CheckboxToggle
+                            label={selector}
+                            checked={selectedAttributes.includes(selector)}
+                            on:change={e=>selectedAttributes = updateGroup(e, selectedAttributes)}
+                        />
+                    </div>
+                {/each}
+            </div>
+        {:else}
+            <button on:click={()=>isActive=true}>
+                @
+            </button>
         {/if}
     </div>
 {/if}
 
 <style>
-    #floatingButton {
+    #hoveringNode-position {
         position: absolute;
+        background-color: rgba(44, 44, 44, 0.452);
+        color: white;
+        z-index: 999999;
+    }
+    #selector {
+        display: flex;
+        flex-direction: column;
+    }
+    .attributes, .classes {
+        display: flex;
+    }
+    hr {width: 100%;}
+    #shadow-cancel {
+        background-color: rgba(0, 0, 0, 0.26);
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: 999999;
     }
 </style>
