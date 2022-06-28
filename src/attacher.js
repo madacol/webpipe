843427c04853857221d03f3579f283800f7556f5
Marco D'Agostini <madacol10@gmail.com>
2022-06-28 13:27:13 +0200

    Removed the execCommand hack, and fixed the `beforeinput` event
    Some rich editor still don't listen to the beforeinput event, but they do listen to the paste event.
    But I'll leave that for the future, I need to rethink how all this *dispatching event* work to only dispatch the exact event required

--- src/attacher.js
+++ src/attacher.js
@@ -31,59 +31,69 @@ export async function update({outerText, idx}) {
 
     if (!node) return console.error(`Couldn't find node with the selector: ${cssSelector}`)
 
-    node.value = outerText
-
-    /**
-     * Dispatch events to trigger the change
-     */
-    node.dispatchEvent(new MouseEvent("mouseover", {bubbles: true}))
-    node.dispatchEvent(new MouseEvent("mousedown", {bubbles: true}))
-    node.dispatchEvent(new MouseEvent("mouseup", {bubbles: true}))
-    node.click()
-    node.dispatchEvent(new InputEvent("focus", {bubbles: true}))
-    node.dispatchEvent(new KeyboardEvent("keydown", {bubbles: true}))
-    node.dispatchEvent(new KeyboardEvent("keyup", {bubbles: true}))
-    node.dispatchEvent(new KeyboardEvent("keypress", {bubbles: true}))
-
-    const data = new DataTransfer()
-    data.setData(
-        'text/html',
-        outerText
-    )
-    node.dispatchEvent(new ClipboardEvent( "paste", {
-        dataType: "text/plain",
-        data: outerText,
-        bubbles: true,
-        clipboardData: data
-    })) // https://github.com/facebook/draft-js/issues/616#issuecomment-426047799
-    node.dispatchEvent(new InputEvent("input", {bubbles: true}))
-    node.dispatchEvent(new InputEvent("beforeinput", {inputType: "inserting", data: outerText, bubbles: true}))
-    node.dispatchEvent(new InputEvent("change", {bubbles: true}))
-    node.dispatchEvent(new InputEvent("blur", {bubbles: true}))
-
-    /**
-     * The following is to make it work on some rich-editors
-     * 
-     * https://github.com/facebook/draft-js/issues/616
-     */
+    selectNodeText(node)
 
     if (node.isContentEditable) {
         /**
-         * First let me send a compassionate message full of love to all poor souls
-         * who had no choice but to use the `contenteditable` attribute,
-         * and to all those like me who are forced to work with it against our will ='(
-         * 
-         * Now let's use a deprecated API yaaayyy (this is needed for firefox which probably will affect chrome too)
-         * https://stackoverflow.com/questions/60581285/execcommand-is-now-obsolete-whats-the-alternative
-         * TL;DR: THERE ARE NO ALTERNATIVES!
-         * 
-         * Now pray for my self from the future who will have to deal with duplicated event triggers
-         * and draftjs braking apart because its really sensitive.
-         * 
-         * Really sorry for this rudeness but, Here we go! <3
+         * The following is to make it work on rich-editors
+         * https://github.com/facebook/draft-js/issues/616
          */
-        selectNodeText(node)
-        document.execCommand('insertText', false, outerText)
+
+        /**
+         * TODO:
+         * Most editors support `beforeinput` and `paste` event,
+         * but some only support one of these.
+         * 
+         * And we cannot use both because text gets duplicated
+         * on those that support both events
+         * 
+         * We should try to test one-by-one on setup,
+         * and only use the one that works
+         * 
+         * And/or let the user manually test each event in the explorer
+         */
+
+        //  const data = new DataTransfer();
+        //  data.setData(
+        //      'text/plain',
+        //      outerText
+        //  );
+        //  node.dispatchEvent(new ClipboardEvent("paste", {
+        //      dataType: "text/plain",
+        //      data: outerText,
+        //      bubbles: true,
+        //      clipboardData: data,
+        //      cancelable: true
+        //  }));  // https://github.com/facebook/draft-js/issues/616#issuecomment-426047799
+
+        document.addEventListener("selectionchange",()=>{
+            node.dispatchEvent(new InputEvent("beforeinput", {
+                inputType: "insertText",
+                data: outerText,
+                bubbles: true,
+                cancelable: true
+            }))
+        },{once: true})
+
+    } else {
+
+        node.value = outerText
+
+        /**
+         * Dispatch events to trigger the change
+         */
+        node.dispatchEvent(new MouseEvent("mouseover", {bubbles: true}))
+        node.dispatchEvent(new MouseEvent("mousedown", {bubbles: true}))
+        node.dispatchEvent(new MouseEvent("mouseup", {bubbles: true}))
+        node.click()
+        node.dispatchEvent(new InputEvent("focus", {bubbles: true}))
+        node.dispatchEvent(new KeyboardEvent("keydown", {bubbles: true}))
+        node.dispatchEvent(new KeyboardEvent("keyup", {bubbles: true}))
+        node.dispatchEvent(new KeyboardEvent("keypress", {bubbles: true}))
+        node.dispatchEvent(new InputEvent("beforeinput", {inputType: "insertText", data: outerText, bubbles: true, cancelable: true}))
+        node.dispatchEvent(new InputEvent("input", {inputType: "insertText", data: outerText, bubbles: true}))
+        node.dispatchEvent(new InputEvent("change", {bubbles: true}))
+        node.dispatchEvent(new InputEvent("blur", {bubbles: true}))
     }
 }
 
